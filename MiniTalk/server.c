91eabfb407ba213ac3248a7221cdb62f79a856a6
sami2004 <86780420+Asami2004@users.noreply.github.com>
2024-02-18 20:34:30 +0100

    Server responds to messages by sending signals back

--- MiniTalk/server.c
+++ MiniTalk/server.c
@@ -6,7 +6,7 @@
 /*   By: stakhtou <stakhtou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 01:13:39 by stakhtou          #+#    #+#             */
-/*   Updated: 2024/02/10 19:17:54 by stakhtou         ###   ########.fr       */
+/*   Updated: 2024/02/18 16:12:18 by stakhtou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,37 @@
 
 struct g_GlobalData	g_lobal_data;
 
-void	siguser1(void)
+void	siguser1(int sig, siginfo_t *info, void *context)
 {
+	(void)sig;
+	(void)context;
 	g_lobal_data.received_byte <<= 1;
 	g_lobal_data.received_bits++;
 	if (g_lobal_data.received_bits == 8)
 	{
+		if (g_lobal_data.received_byte == '\0')
+		{
+			kill(info->si_pid, SIGUSR1);
+		}
 		ft_printf("%c", g_lobal_data.received_byte);
 		g_lobal_data.received_byte = 0;
 		g_lobal_data.received_bits = 0;
 	}
 }
 
-void	siguser2(void)
+void	siguser2(int sig, siginfo_t *info, void *context)
 {
+	(void)sig;
+	(void)context;
 	g_lobal_data.received_byte <<= 1;
 	g_lobal_data.received_byte |= 1;
 	g_lobal_data.received_bits++;
 	if (g_lobal_data.received_bits == 8)
 	{
+		if (g_lobal_data.received_byte == '\0')
+		{
+			kill(info->si_pid, SIGUSR2);
+		}
 		ft_printf("%c", g_lobal_data.received_byte);
 		g_lobal_data.received_byte = 0;
 		g_lobal_data.received_bits = 0;
@@ -44,12 +56,19 @@ void	siguser2(void)
 
 int	main(void)
 {
-	if (signal(SIGUSR1, siguser1) == SIG_ERR)
+	struct sigaction	sa1;
+	struct sigaction	sa2;
+
+	sa1.sa_sigaction = siguser1;
+	sa2.sa_sigaction = siguser2;
+	sa1.sa_flags = SA_SIGINFO;
+	sa2.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa1, NULL) == -1)
 	{
 		ft_printf("Unable to register SIGUSR1 handler");
 		exit(1);
 	}
-	if (signal(SIGUSR2, siguser2) == SIG_ERR)
+	if (sigaction(SIGUSR2, &sa2, NULL) == -1)
 	{
 		ft_printf("Unable to register SIGUSR2 handler");
 		exit(1);
