d3bd160296f24fb1274b2fb5f1d018e46fe5fbed
Sami Takhtoukh <stakhtou@e1r6p7.1337.ma>
2024-03-19 01:34:58 +0000

    so l

---
+++ so long/1337Libft/README.md
@@ -0,0 +1,50 @@
+# libft 42
+libft 100/100
+
+## Description
+Libft is a 42 project that involves creating your own C library,
+containing re-implementations of standard functions, 
+as well as additional functions that may be useful.
+
+## Table of Contents
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Documentation](#documentation)
+- [Contributing](#contributing)
+
+## Getting Started
+
+### Prerequisites
+Before you begin, make sure you have the following installed:
+- GCC compiler
+- Make
+
+### Installation
+1. Clone the repository to your local machine:
+ ```c
+git clone https://github.com/Asami2004/42-1337-projects.git
+```
+ ```c
+cd libft
+```
+ ```c
+make
+```
+Your libft library is now ready to be used in your projects.
+
+# Usage
+To use the library in your project, include the libft.h header file and link against the compiled library.
+
+Example compilation:
+ ```c
+ gcc -o my_program my_program.c -L. -lft
+```
+# Documentation
+
+For details on the functions provided by this library, refer to the libft.h header file.
+
+# Contributing
+
+If you would like to contribute to this project, feel free to open issues or submit pull requests
---
+++ so long/checkmaps.c
@@ -0,0 +1,147 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkmaps.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakhtou <stakhtou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 19:46:32 by stakhtou          #+#    #+#             */
+/*   Updated: 2024/03/19 01:30:27 by stakhtou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "solong.h"
+
+char	*file_to_str(int fd)
+{
+	char	*line;
+	char	*ptr;
+	char	*tmp;
+	size_t	total_len;
+	size_t	len;
+
+	ptr = NULL;
+	total_len = 0;
+	line = get_next_line(fd);
+	while ((line) != NULL)
+	{
+		len = strlen(line);
+		tmp = ptr;
+		ptr = malloc(total_len + len + 1);
+		if (tmp)
+		{
+			ft_memcpy(ptr, tmp, total_len);
+			free(tmp);
+		}
+		ft_memcpy(ptr + total_len, line, len);
+		total_len += len;
+		ptr[total_len] = '\0';
+		line = get_next_line(fd);
+	}
+	return (ptr);
+}
+
+int	validate_map(const char *map)
+{
+	t_map_info	info;
+	const char	*ptr = map;
+
+	info.exit_pos = ft_strchr(map, 'E');
+	info.collectible_pos = ft_strchr(map, 'C');
+	info.start_pos = ft_strchr(map, 'P');
+	info.zero_pos = ft_strchr(map, '0');
+	info.one_pos = ft_strchr(map, '1');
+	info.exit_count = 0;
+	while (*ptr != '\0')
+	{
+		if (*ptr == 'E' || *ptr == 'P')
+			info.exit_count++;
+		ptr++;
+	}
+	if (info.exit_pos == NULL || info.collectible_pos == NULL
+		|| info.start_pos == NULL || info.zero_pos == NULL
+		|| info.one_pos == NULL || info.exit_count > 2)
+		return (0);
+	return (1);
+}
+
+int	rect_check(const char *filename)
+{
+	int		fd;
+	char	*line;
+	int		length;
+	int		line_num;
+	int		first_len;
+
+	line_num = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		length = 0;
+		while (line[length] != '\0' && line[length] != '\n')
+			length++;
+		if (line_num == 0)
+			first_len = length;
+		else if (length != first_len)
+			return (0);
+		line = get_next_line(fd);
+		line_num++;
+	}
+	close(fd);
+	return (1);
+}
+
+void	all_func(const char *filename)
+{
+	t_data			data;
+	t_flood_fill	fill;
+	int				i;
+
+	fill.arg = filename;
+	data.our_r = rect_check(filename);
+	data.close = check_close(filename);
+	fill.map = convert_map(&fill);
+	if (data.our_r == 0 || data.close == 0 || check_go(&fill) == 0
+		|| eat_check(fill) == 0)
+	{
+		ft_printf("Error\n");
+		return ;
+	}
+	else
+		ft_printf("all good\n");
+	i = 0;
+	while (i < fill.rows)
+	{
+		free(fill.map[i]);
+		i++;
+	}
+	free(fill.map);
+}
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	t_data	data;
+	int		val;
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+			return (1);
+		data.convert = file_to_str(fd);
+		val = validate_map(data.convert);
+		if (val == 0)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		all_func(av[1]);
+		return (0);
+	}
+	else if (ac != 2)
+		return (0);
+}
